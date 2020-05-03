ab61b308a8eff436d9d6d78716dcd0b516a579e0
GiselleDZ <giselle@gisellezatonyl.com>
2020-05-03 17:23:53 -0400

    posenet works w standard size camera

--- client/app.js
+++ client/app.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import PoseNet from './components/Posenet/PoseNet'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <PoseNet />
       <Routes />
     </div>
   )
