e4747a7d97e2bacad571b78d95aa9d0ee477a007
GiselleDZ <giselle@gisellezatonyl.com>
2020-05-03 18:51:15 -0400

    camera works and properly resizes

--- client/app.js
+++ client/app.js
@@ -3,13 +3,15 @@ import React from 'react'
 import {Navbar} from './components'
 import Routes from './routes'
 import PoseNet from './components/Posenet/PoseNet'
+import WindowResize from './components/Posenet/WindowResize'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <PoseNet />
-      <Routes />
+      {/* <Navbar /> */}
+      {/* <PoseNet /> */}
+      <WindowResize />
+      {/* <Routes /> */}
     </div>
   )
 }
