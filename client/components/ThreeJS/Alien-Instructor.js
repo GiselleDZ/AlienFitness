066f6265b30b1bccb0cfb7af1ffb29879686423c
GiselleDZ <giselle@gisellezatonyl.com>
2020-05-03 23:11:44 -0400

    hosted 3d model, backingup

--- client/components/ThreeJS/Alien-Instructor.js
+++ client/components/ThreeJS/Alien-Instructor.js
@@ -3,7 +3,7 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 var loader = new THREE.GLTFLoader()
 
 loader.load(
-  '../../models/Alien-Doing-JumpingJacks.glb',
+  'http://gisellezatonyl.com/models/Alien-Doing-JumpingJacks.glb',
   function(gltf) {
     scene.add(gltf.scene)
   },
